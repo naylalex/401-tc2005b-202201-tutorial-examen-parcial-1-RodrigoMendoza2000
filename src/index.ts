f0c590eb72d34b736466135a7f7746b78874a28f
Rodrigo Mendoza <58440545+RodrigoMendoza2000@users.noreply.github.com>
2022-03-28 04:38:04 +0000

    actividad

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,11 @@ function buttonSubmitClick() {
     const divResult = <HTMLDivElement>document.getElementById('divResult');
     if (divResult) {
         if (bPalindrome) {
-            divResult.innerText = 'The word is palindrome!'
+            divResult.innerText = 'The word is palindrome!3'
             //TODO: Implementa la asignación del atributo className con valor 'palindrome'
+            divResult.className = "palindrome";
         } else {
+            divResult.innerText = 'The word is not palindrome, sorry.'
             //TODO: Asigna a la propiedad innerText de divResult el valor 'The word is not palindrome, sorry.'
             divResult.className = "notPalindrome";
         }
@@ -30,6 +32,7 @@ const btn = document.querySelector('button');
 if (btn) {
   console.log('attaching listener');
   //TODO: invoca la función addEventListener de btn con parámetros 'click' y buttonSubmitClick
+  btn.addEventListener('click', buttonSubmitClick);
 }
 
 
